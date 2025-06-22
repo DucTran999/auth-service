722fdc81ab8269ca965e93459fa2907f3f32f04f
Đức Trần <tranaduc9x@gmail.com>
2025-06-22 18:57:48 +0700

    test: new builder for setup mock impl

---
+++ test/mock-builder/hashser.go
@@ -0,0 +1,45 @@
+package mockbuilder
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/DucTran999/auth-service/test/mocks"
+	"github.com/stretchr/testify/mock"
+)
+
+var (
+	ErrCompareHashPassword = errors.New("compare password unexpected error")
+)
+
+type mockHasherBuilder struct {
+	inst *mocks.Hasher
+}
+
+func newMockHasherBuilder(t *testing.T) *mockHasherBuilder {
+	return &mockHasherBuilder{
+		inst: mocks.NewHasher(t),
+	}
+}
+
+func (b *mockHasherBuilder) GetInstance() *mocks.Hasher {
+	return b.inst
+}
+
+func (b *mockHasherBuilder) HashPasswordMatch() {
+	b.inst.EXPECT().
+		ComparePasswordAndHash(mock.Anything, mock.AnythingOfType("string")).
+		Return(true, nil)
+}
+
+func (b *mockHasherBuilder) HashPasswordNotMatch() {
+	b.inst.EXPECT().
+		ComparePasswordAndHash(mock.AnythingOfType("string"), mock.Anything).
+		Return(false, nil)
+}
+
+func (b *mockHasherBuilder) HashPasswordGotError() {
+	b.inst.EXPECT().
+		ComparePasswordAndHash(mock.AnythingOfType("string"), mock.Anything).
+		Return(false, ErrCompareHashPassword)
+}
